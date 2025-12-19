01dce23d68a0874f101fc289877f780821b94e92
RichardJhn <rjohn@my.yearupunited.org>
2025-12-19 00:53:41 -0500

    quick

--- src/main/java/org/yearup/controllers/ShoppingCartController.java
+++ src/main/java/org/yearup/controllers/ShoppingCartController.java
@@ -87,12 +87,9 @@ public class ShoppingCartController
 
     }
 
-
-
     // add a PUT method to update an existing product in the cart - the url should be
     // https://localhost:8080/cart/products/15 (15 is the productId to be updated)
     // the BODY should be a ShoppingCartItem - quantity is the only value that will be updated
-
 
     // add a DELETE method to clear all products from the current users cart
     // https://localhost:8080/cart
